f9400fc0f20cf16c89f8eb56c91c0321f3c950a4
KonurbaevaTN <konurbaeva_tn@edu.surgu.ru>
2022-02-16 16:04:03 +0500

    help

---
+++ lab1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  float a, b, c;
+  printf("Enter a -> ");
+  scanf("%f", &a);
+  printf("Enter b -> ");
+  scanf("%f", &b);
+  printf("Enter c -> ");
+  scanf("%f", &x);
+  if (a > b)
+  return 0;
+  float x = 1.0f;
+  printf();
+  if ()
+  {
+    print
+  }
+
+}
